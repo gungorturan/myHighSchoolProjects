314cd9b15f2717e1cc6bcc0e06aa569335c6c915
Güngör Turan <turangungor1@gmail.com>
2018-04-09 21:27:39 +0300

    Dokanma Yanarsın

---
+++ DokanmaYanarsın/DokanmaYanarsın/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace DokanmaYanarsın
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(2, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 204);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DOKANMA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(83, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DOKANMA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.Location = new System.Drawing.Point(83, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "DOKANMA";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button4.Location = new System.Drawing.Point(83, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "DOKANMA";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button5.Location = new System.Drawing.Point(83, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 31);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "DOKANMA";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.MouseEnter += new System.EventHandler(this.button5_MouseEnter);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button6.Location = new System.Drawing.Point(33, 213);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(204, 34);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "DOKANMA";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.MouseEnter += new System.EventHandler(this.button6_MouseEnter);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button7.Location = new System.Drawing.Point(234, -54);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(35, 232);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "DOKANMA";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.MouseEnter += new System.EventHandler(this.button7_MouseEnter);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button8.Location = new System.Drawing.Point(302, 43);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(35, 204);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "DOKANMA";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.MouseEnter += new System.EventHandler(this.button8_MouseEnter);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button9.Location = new System.Drawing.Point(234, 213);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(71, 34);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "DOKANMA";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.MouseEnter += new System.EventHandler(this.button9_MouseEnter);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button10.Location = new System.Drawing.Point(334, 43);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(176, 36);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "DOKANMA";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.MouseEnter += new System.EventHandler(this.button10_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(694, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "BİTİŞ";
+            this.label1.MouseEnter += new System.EventHandler(this.label1_MouseEnter);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button12.Location = new System.Drawing.Point(562, 1);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(38, 246);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "DOKANMA";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button13.Location = new System.Drawing.Point(391, 116);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(175, 38);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "DOKANMA";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button14.Location = new System.Drawing.Point(334, 209);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(175, 38);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "DOKANMA";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button15.Location = new System.Drawing.Point(644, 43);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(38, 234);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "DOKANMA";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(780, 279);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+    }
+}
+
---
+++ DokanmaYanarsın/DokanmaYanarsın/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DokanmaYanarsın
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+        private void button10_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tebrikler...");
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button4.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Cursor.Position = new Point(2, 43);
+
+
+            
+            Bitmap bmp = new Bitmap(new Bitmap(Properties.Resources.bugs_bunny___Kopya), 25, 25);
+
+
+            Cursor yenıCursor = new Cursor(bmp.GetHicon());
+            this.Cursor = yenıCursor;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button5_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button6_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button7_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button8_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button9_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(2, 43);
+        }
+    }
+}
