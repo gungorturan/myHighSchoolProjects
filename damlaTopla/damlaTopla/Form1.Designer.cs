98e82c8bd008f5f5a93edc50f01c333337ff506f
Güngör Turan <turangungor1@gmail.com>
2018-04-09 21:25:50 +0300

    Damla Toplama

---
+++ damlaTopla/damlaTopla/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace damlaTopla
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_kova = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.damlalarOlustur = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_kova
+            // 
+            this.btn_kova.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_kova.BackgroundImage")));
+            this.btn_kova.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_kova.Location = new System.Drawing.Point(115, 300);
+            this.btn_kova.Name = "btn_kova";
+            this.btn_kova.Size = new System.Drawing.Size(60, 60);
+            this.btn_kova.TabIndex = 0;
+            this.btn_kova.UseVisualStyleBackColor = true;
+            this.btn_kova.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btn_kova_KeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "Damla.gif");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(500, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // damlalarOlustur
+            // 
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_kova);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_kova;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer damlalarOlustur;
+    }
+}
+
---
+++ damlaTopla/damlaTopla/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace damlaTopla
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        Button damla = new Button();
+        int damlaKonY = 0;
+        int damlaKonX;
+        bool first = true;
+        int skor = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void damlaOlustur()
+        {
+            damlaKonX = rnd.Next(0, 400);
+            damla.Name = "damla";
+            damla.Location = new Point(damlaKonX, damlaKonY);
+            damla.Size = new Size(20, 20);
+            damla.Image = new Bitmap(ımageList1.Images[0]);
+            this.Controls.Add(damla);
+        }
+
+
+        private void btn_kova_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    if (btn_kova.Left > 0)
+                    {
+                        btn_kova.Location = new Point(btn_kova.Location.X - 5, btn_kova.Location.Y);
+                    }
+                    break;
+                case Keys.D:
+                    if (btn_kova.Right < this.Width - 15)
+                    {
+                        btn_kova.Location = new Point(btn_kova.Location.X + 5, btn_kova.Location.Y);
+                    }
+                    
+                    break;
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (first == true)
+            {
+                damlaOlustur();
+                first = false;
+            }
+            if (damla.Bottom < btn_kova.Top || damla.Bottom < this.Height)
+            {
+                damlaKonY += 5;
+                damla.Location = new Point(damlaKonX, damlaKonY);
+            }
+            if (damla.Bottom > btn_kova.Top && damla.Right < btn_kova.Right && damla.Left > btn_kova.Left)
+            {
+                damlaKonY = 0;
+                damlaKonX = rnd.Next(0, 400);
+                damla.Location = new Point(damlaKonX, damlaKonY);
+                skor += 1;
+
+            }
+            if (damla.Bottom > this.Height - 50)
+            {
+                damlaKonY = 0;
+                damlaKonX = rnd.Next(0, 400);
+                damla.Location = new Point(damlaKonX, damlaKonY);
+            }
+            label1.Text = "Skor: " + skor;
+            switch (skor)
+            {
+                case 5:
+                    timer1.Interval = 75;
+                    break;
+                case 20:
+                    timer1.Interval = 50;
+                    break;
+                case 50:
+                    timer1.Interval = 25;
+                    break;
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            label1.Text += skor;
+        }
+    }
+}
