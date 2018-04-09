b8a46228e6a0bfdb13f2450ebd6c7d62567180b0
Güngör Turan <turangungor1@gmail.com>
2018-04-09 21:24:14 +0300

    Basit Slayt

---
+++ checkbox picturebox/checkbox picturebox/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace checkbox_picturebox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.chclbox = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chclbox
+            // 
+            this.chclbox.FormattingEnabled = true;
+            this.chclbox.Location = new System.Drawing.Point(53, 36);
+            this.chclbox.Name = "chclbox";
+            this.chclbox.Size = new System.Drawing.Size(157, 154);
+            this.chclbox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Çek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(253, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Durdur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 299);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chclbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox chclbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ checkbox picturebox/checkbox picturebox/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkbox_picturebox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Eğer gözat sekmesinde bir şeyi seçtiğinde onu ekler.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                chclbox.Items.Add(openFileDialog1.FileName);  
+            }
+        }
+        //sayaç
+        int s = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //checklistteki sayı s'den büyükse checklistteki seçilmiş nesnelerin indexleriyle picturebox'a resimleri ekler.
+            if (chclbox.CheckedItems.Count>=s)
+            {
+                pictureBox1.Image = new Bitmap(Convert.ToString(chclbox.CheckedItems[s]));
+                s++;
+                //sayaç sayısı count sayısına ulaştığında sayacı başa alır ve böylece resimleri sonsuza dek oynatır.
+                if(s==chclbox.CheckedItems.Count)
+                {
+                    s = 0;
+                }
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //checklistte seçim yapılmazsa uyarı veriyor.
+            if(chclbox.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("Bir veya birden fazla seçim yapın!!!");
+            }
+            //resimleri belli aralıklarla oynatmak için timerı başlatır.
+            timer1.Enabled = true;
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //timerı durdurur.
+            timer1.Enabled = false;
+        }
+    }
+}
+//evde çoklu seçme olayını yap.
