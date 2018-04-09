6be58c2b1cd68a877079e40306bd0a592449d530
Güngör Turan <turangungor1@gmail.com>
2018-04-09 21:21:52 +0300

    Sinavim

---
+++ 455-Güngör TURAN/455-GüngörTuran/455-GüngörTuran/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace _455_GüngörTuran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(113, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sıralama";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Karışık";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Sıralı";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox5);
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Location = new System.Drawing.Point(400, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(166, 337);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ayarlar";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.radioButton5);
+            this.groupBox5.Controls.Add(this.radioButton6);
+            this.groupBox5.Location = new System.Drawing.Point(6, 267);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(113, 63);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Şeffaflık";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(3, 39);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(53, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Şeffaf";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(3, 16);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(63, 17);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Görünür";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Location = new System.Drawing.Point(6, 205);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(113, 56);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Renk";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Controls.Add(this.radioButton4);
+            this.groupBox3.Location = new System.Drawing.Point(6, 112);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(113, 87);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Seçim";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 53);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(92, 17);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Manuel Seçim";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(3, 16);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(99, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Otomatik Seçim";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "1.png");
+            this.ımageList1.Images.SetKeyName(1, "2.png");
+            this.ımageList1.Images.SetKeyName(2, "3.png");
+            this.ımageList1.Images.SetKeyName(3, "4.png");
+            this.ımageList1.Images.SetKeyName(4, "5.png");
+            this.ımageList1.Images.SetKeyName(5, "6.png");
+            this.ımageList1.Images.SetKeyName(6, "7.png");
+            this.ımageList1.Images.SetKeyName(7, "8.png");
+            this.ımageList1.Images.SetKeyName(8, "9.png");
+            this.ımageList1.Images.SetKeyName(9, "10.png");
+            this.ımageList1.Images.SetKeyName(10, "11.png");
+            this.ımageList1.Images.SetKeyName(11, "12.png");
+            this.ımageList1.Images.SetKeyName(12, "13.png");
+            this.ımageList1.Images.SetKeyName(13, "14.png");
+            this.ımageList1.Images.SetKeyName(14, "15.png");
+            this.ımageList1.Images.SetKeyName(15, "16.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 357);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "455-Güngör TURAN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+
---
+++ 455-Güngör TURAN/455-GüngörTuran/455-GüngörTuran/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _455_GüngörTuran
+{
+    public partial class Form1 : Form
+    {
+        int konx = 5;
+        int kony = 5;
+        int sayi;
+        Random rnd = new Random();
+        int tik = 0;
+        PictureBox secilenpbx, secilenpbx2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void pbxOlustur()
+        {
+            for(int i = 1; i < 17; i++)
+            {
+                PictureBox pbx = new PictureBox();
+                pbx.Name = "pictureBox" + i;
+                pbx.Width = 75;
+                pbx.Height = 75;
+                pbx.Location = new Point(konx, kony);
+                pbx.Click += Pbx_Click;
+                this.Controls.Add(pbx);
+                if (konx < 230)
+                {
+                    konx += 80;
+                }
+                else
+                {
+                    konx = 5;
+                    kony += 80;
+                }
+            }
+        }
+        private void Pbx_Click(object sender, EventArgs e)
+        {
+            PictureBox pbx = (PictureBox)sender;
+            if(tik == 0)
+            {
+                tik++;
+            }
+            if(tik == 1)
+            {
+                secilenpbx = pbx;
+                tik++;
+            }
+            else if(tik == 2)
+            {
+                secilenpbx2 = pbx;
+                secilenpbx2.Image = secilenpbx.Image;
+                pbx.Image = secilenpbx2.Image;
+                tik = 0;
+            }
+        }
+
+        private void siraliResimYukle()
+        {
+            int[] sayilar = new int[16];
+            for (int i = 0; i < 16; i++)
+            {
+                ((PictureBox)this.Controls["pictureBox" + (i + 1)]).Image = ımageList1.Images[i];
+            }
+        }
+        private void karisikResimYukle()
+        {
+            int[] sayilar = new int[16];
+            for(int i = 0; i < 16; i++)
+            {
+                do
+                {
+                    sayi = rnd.Next(0, 17);
+                } while (Array.IndexOf(sayilar, sayi) != -1);
+                sayilar[i] = sayi;
+            }
+            for (int i = 0; i < 16; i++)
+            {
+                ((PictureBox)this.Controls[sayilar[i]]).Image = ımageList1.Images[i];
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbxOlustur();
+            siraliResimYukle();
+            comboBox1.Items.Add("Sarı");
+            comboBox1.Items.Add("Lacivert");
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            siraliResimYukle();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pbxOlustur();
+            karisikResimYukle();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else if(comboBox1.SelectedIndex == 1)
+            {
+                this.BackColor = Color.DarkBlue;
+            }
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Opacity = 0.5;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            siraliResimYukle();
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+        }
+    }
+}
